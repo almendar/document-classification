4e945e41fc6d73c34a1adfc5757c1c2b32cf4c31
almendar <almendar@97adb64c-0b1a-efa1-e6d2-ff562096f319>
2011-01-02 19:52:41 +0000

    Initial commit with stub class.

---
+++ document-classification/trunk/document-classification/Stub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace document_classification
+{
+    /// <summary>
+    /// This class is just a stub.
+    /// Generated for SVN initialization only.
+    /// Will be deleted in future.
+    /// </summary>
+    public class Stub
+    {
+
+    }
+}
