1a7b95ffe67242c0bb6466dbc68cb658cbbbd82e
almendar <almendar@97adb64c-0b1a-efa1-e6d2-ff562096f319>
2011-01-14 21:34:03 +0000

    Added: - TextExtraction class stub - tkogut testing console app

---
+++ document-classification/trunk/document-classification/TextExtraction.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace document_classification
+{
+   public  class TextExtraction
+    {
+    }
+}
---
+++ document-classification/trunk/tkogutTestApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using document_classification;
+
+
+namespace tkogutTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] splitChars = new char[] {
+                ' ',
+                ',',
+                '.',
+                '\n'};
+
+
+            String text = ".aklsadkla fclaksc a;vcas;c.a';.cvac as c as c as ckasckla slkc aslkc aslkc     asc.";
+            text = text.Trim();
+
+            String [] literals = text.Split(splitChars,StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (String s in literals)r
+            {
+                Console.Out.WriteLine("#"+s+"#");
+
+            }
+
+            Console.ReadKey
+                ();
+        }
+
+
+    }
+}
