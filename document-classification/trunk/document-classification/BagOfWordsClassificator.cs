2982ddcd3b3d7e76a4f71d076ed842413cf23901
almendar <almendar@97adb64c-0b1a-efa1-e6d2-ff562096f319>
2011-01-16 11:04:16 +0000

    Update

--- document-classification/trunk/document-classification/BagOfWordsClassificator.cs
+++ document-classification/trunk/document-classification/BagOfWordsClassificator.cs
@@ -9,44 +9,55 @@ namespace document_classification
     /// </summary>
     class BagOfWordsTextClassifier
     {
-        static readonly BagOfWordsTextClassifier instance = new BagOfWordsTextClassifier();
-        static BagOfWordsTextClassifier()  
-        {
-        }
 
+        #region fields
+        private int                          NumberOfCases;
         private DBRepresentation             allWords = null;
         private AllCases                     CasesSet = null;
+        private AllProcedures                ProceduresSet = null;
         private DecisionRepresentationPhase  PhaseDecisionData = null;
         private DecisionRepresentationPeople PeopleDecisionData = null;
+        
 
-        //---------------------------------------------------------------//
+        /// <summary>
+        /// Threashold above which words are not taken into consideration.
+        /// When wors appears in more documents that trheashold stand for
+        /// word is being omited
+        /// </summary>
+        private int wordThreashold = int.MaxValue;
+
+        /// <summary>
+        /// Maps word to column number.
+        /// Take into consideration that not evey word is present because of the threashold of the TFIDF.
+        /// </summary>
+        private Dictionary<String, int> MapWordToColumn = null;
+
+        //**********************************************************************//
         /// <summary>
         /// Rows of this matrix represents procedures, columns are words.
         /// Values of this matrix are TFIDF of words in procedures.
         /// </summary>
-        private double[][] ProcedureMatrix  = null;
+        private double[,] ProcedureMatrix  = null;
         /// <summary>
         /// Table maps row of the <see cref="ProcedureMatrix"/>procedure matrix</see> to correspondent procedure id 
         /// in the database.
         /// </summary>
         private int[] MapRowToProcedureId = null;
-        /// <summary>
-        /// Maps word to column number.
-        /// Take into consideration that not evey word is present because of the threashold of the TFIDF.
-        /// </summary>
-        private Dictionary<String,int> MapWordToColumn = null;
-        /// <summary>
-        /// Threashold below which words are not taken into consideration.
-        /// </summary>
-        private double TFIDFThreshold = Double.NegativeInfinity;
 
-        //---------------------------------------------------------------//
-        private double[][][] PhaseMatrix      = null;
-        private int[] MapHeightToNextPhaseId = null;
+        //**********************************************************************//
+        private double[,,,] PhaseMatrix      = null;
+        private int[,,] MapRowColumnToNextPhaseId = null;
 
-        //---------------------------------------------------------------//
-        private double[][][] PeopleMatrix     = null;
-        private int[] MapHeightToNextPersonId = null;
+        //**********************************************************************//
+        private double[,,,] PeopleMatrix     = null;
+        private int[,,] MapRowColumnToNextPersonId = null;
+
+        static readonly BagOfWordsTextClassifier instance = new BagOfWordsTextClassifier();
+#endregion
+
+        static BagOfWordsTextClassifier()
+        {
+        }
 
         BagOfWordsTextClassifier()
         {
@@ -93,6 +104,68 @@ namespace document_classification
             throw new NotImplementedException();
         }
 
+
+
+        private void ProcedureMatrixBuild()
+        {
+            int numberOfProcedures = ProceduresSet.Keys.Count;
+            int nrOfMeaningfulWords = MapWordToColumn.Keys.Count;//vector length
+            int nrOfProcedures = ProceduresSet.Keys.Count;
+            ProcedureMatrix = new double[numberOfProcedures, nrOfMeaningfulWords];
+            int procedurIndex = 0;
+            foreach (KeyValuePair<int, Procedure> kvp in ProceduresSet)
+            {
+                int procedurId = kvp.Key;
+                Procedure currentProcedure = kvp.Value;
+                MapRowToProcedureId[procedurIndex] = procedurId;
+                foreach (KeyValuePair<string, double> procedHashMap in currentProcedure)
+                {
+                    string word = procedHashMap.Key;
+                    double TFIDF = procedHashMap.Value;
+                    if (!MapWordToColumn.ContainsKey(word))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        int wordIndex = MapWordToColumn[word];
+                        ProcedureMatrix[procedurIndex,wordIndex] = TFIDF;
+                    }
+                }
+                ++procedurIndex; //step to next procedure
+            }
+
+            
+        }
+
+        private void NextPhaseMatrixBuild()
+        {
+        }
+
+        private void NextPersonMatrixBuild()
+        {
+        }
+
+        /// <summary>
+        /// Finds all words that frequency in documents is less than threashold
+        /// </summary>
+        private void FetchMeaningfulWords()
+        {
+            int numberOfProcedures = ProceduresSet.Keys.Count;
+            this.wordThreashold = (numberOfProcedures * 9) / 10;
+            int vectorIndice = 0;
+            foreach(KeyValuePair<string, int> kvp in allWords)
+            {
+                int documentFrequency = kvp.Value;
+                string word = kvp.Key;
+                if (documentFrequency <= wordThreashold)
+                {
+                    MapWordToColumn.Add(word, vectorIndice);
+                    ++vectorIndice;
+                }
+
+            }
+        }
         
     }
 }
